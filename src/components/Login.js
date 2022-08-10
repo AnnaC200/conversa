1b3e0fec328738a72c5b762d981e8580335c9cd6
khadra mohamed <khadramohamed@live.co.uk>
2022-08-10 17:06:40 +0100

    edited login

--- src/components/Login.js
+++ src/components/Login.js
@@ -11,17 +11,30 @@ const Login = () => {
   const handleLogin = (event) => {
     event.preventDefault();
   };
+  const handleFieldChange = (event) => {
+    setFields({ ...fields, [event.target.name]: event.target.value });
+  };
   return (
     <div className="login">
       Login
       <form onSubmit={handleLogin}>
         <label className="login-items" htmlFor="UserName">
           UserName
-          <input id="UserName" name="UserName" value={fields.UserName} />
+          <input
+            id="UserName"
+            name="UserName"
+            value={fields.UserName}
+            onChange={handleFieldChange}
+          />
         </label>
         <label className="login-items" htmlFor="Password">
           Password
-          <input id="Password" name="Password" value={fields.Password} />
+          <input
+            id="Password"
+            name="Password"
+            value={fields.Password}
+            onChange={handleFieldChange}
+          />
         </label>
       </form>
     </div>
