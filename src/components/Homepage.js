3d902d00817895fff21963753233578e0c09a9c0
Ash Askin <ashaskincodes@gmail.com>
2022-08-11 16:28:53 +0100

    Homepage styling done

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,8 +1,11 @@
 import React from "react";
 import "../styles/homepage.css";
+import "../styles/font.css";
 import { Link } from "react-router-dom";
 // import { createBrowserHistory } from "history";
-import image from "../image/conversalogolargecolour.png";
+import peopletalking from "../image/peopletalking.png";
+import image from "../image/logocolour.png";
+import background from "../image/languages.png";
 // import Login from "./Login";
 // import Register from "./Register";
 
@@ -14,33 +17,44 @@ const Homepage = () => {
   //     createBrowserHistory.push("/Register");
   //   };
   return (
-    <div className="homepage">
-      <ul>
+    <body>
+      <div className="homepage">
+        <img className="background" src={background} alt="background" />
         <img className="logo" src={image} alt="conversa-logo" />
-        <Link to="/">
-          <button
-            // onClick={NavigateToLogin}
-            className="homepage-buttons"
-            type="submit"
-          >
-            Login
-          </button>
-        </Link>
-        <br />
-        <br />
-        <br />
-        <br />
-        <Link to="/Register">
-          <button
-            // onClick={NavigateToRegister}
-            className="homepage-buttons"
-            type="submit"
-          >
-            Register
-          </button>
-        </Link>
-      </ul>
-    </div>
+        <div className="buttons">
+          <Link to="/">
+            <button
+              // onClick={NavigateToLogin}
+              className="homepage-button1"
+              type="submit"
+            >
+              Sign In
+            </button>
+          </Link>
+          <Link to="/Register">
+            <button
+              // onClick={NavigateToRegister}
+              className="homepage-button2"
+              type="submit"
+            >
+              Register
+            </button>
+          </Link>
+        </div>
+        <div>
+          <p className="slogans">
+            Get involved
+            <br />
+            Meet up
+            <br />
+            Exchange languages
+          </p>
+        </div>
+        <div className="peopletalking">
+          <img src={peopletalking} alt="people talking" />
+        </div>
+      </div>
+    </body>
   );
 };
 
