d4f1ab49e54c557606170d60aab34b6cbd0a4fe2
Ash Askin <ashaskincodes@gmail.com>
2022-08-16 13:40:56 +0100

    changed link text colour to black, removed browser default stylings

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import "../styles/register.css";
 import { Link } from "react-router-dom";
+import image from "../image/logocolour.png";
+import background from "../image/languages.png";
 
 const Register = () => {
   const initialState2 = {
@@ -18,46 +20,52 @@ const Register = () => {
     setFields({ ...fields, [event.target.name]: event.target.value });
   };
   return (
-    <div className="register">
-      <form onSubmit={handleRegister}>
-        <label className="register-items" htmlFor="UserName">
-          UserName
-          <input
-            id="UserName"
-            name="UserName"
-            value={fields.UserName}
-            onChange={handleFieldChange}
-          />
-        </label>
-        <label className="register-items" htmlFor="Email">
-          Email
-          <input
-            id="Email"
-            name="Email"
-            value={fields.Email}
-            onChange={handleFieldChange}
-          />
-        </label>
-        <label className="register-items" htmlFor="Password">
-          Password
-          <input
-            id="Password"
-            name="Password"
-            value={fields.Password}
-            onChange={handleFieldChange}
-          />
-        </label>
-        <button className="register-items" type="submit">
-          Register
-        </button>
-      </form>
-      <br />
-      <div>
-        <Link className="link-to" to="/Login">
-          Already have an account?
-        </Link>
+    <>
+      <div className="images">
+        <img className="background" src={background} alt="background" />
+        <img className="logo" src={image} alt="conversa-logo" />
       </div>
-    </div>
+      <div className="register">
+        <form onSubmit={handleRegister}>
+          <label className="register-items" htmlFor="UserName">
+            Username
+            <input
+              id="UserName"
+              name="UserName"
+              value={fields.UserName}
+              onChange={handleFieldChange}
+            />
+          </label>
+          <label className="register-items" htmlFor="Email">
+            Email
+            <input
+              id="Email"
+              name="Email"
+              value={fields.Email}
+              onChange={handleFieldChange}
+            />
+          </label>
+          <label className="register-items" htmlFor="Password">
+            Password
+            <input
+              id="Password"
+              name="Password"
+              value={fields.Password}
+              onChange={handleFieldChange}
+            />
+          </label>
+          <button className="register-items" type="submit">
+            Register
+          </button>
+        </form>
+        <br />
+        <div>
+          <Link className="link-to" to="/Login">
+            Already have an account?
+          </Link>
+        </div>
+      </div>
+    </>
   );
 };
 
