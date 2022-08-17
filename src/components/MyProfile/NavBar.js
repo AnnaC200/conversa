b748adc8586aabf16ced41e0405cdb9157b68430
Ash Askin <ashaskincodes@gmail.com>
2022-08-17 15:02:13 +0100

    Navbar started

--- src/components/MyProfile/NavBar.js
+++ src/components/MyProfile/NavBar.js
@@ -1,7 +1,34 @@
 import React from "react";
+import "./NavBar.css";
+import logo from "../../image/logoblack.png";
 
 const NavBar = () => {
-  return <div className="navbar">hi</div>;
+  return (
+    <div>
+      <nav>
+        <div id="logo">
+          <img className="logopic" src={logo} alt="conversa logo in black" />
+        </div>
+        <label htmlFor="drop" className="toggle">
+          Menu
+          <input type="checkbox" id="drop" />
+        </label>
+        <ul className="menu">
+          <li>
+            <label htmlFor="drop-1" className="toggle">
+              Service +<input type="checkbox" id="drop-1" />
+            </label>
+            Service
+            <ul>
+              <li>Service 1</li>
+              <li>Service 2</li>
+              <li>Service 3</li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default NavBar;
