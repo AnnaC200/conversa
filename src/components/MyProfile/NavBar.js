86fd14631e3ee2b86130e76503b6a8eaba80add6
Ash Askin <ashaskincodes@gmail.com>
2022-08-17 16:34:17 +0100

    navbar styles removed

--- src/components/MyProfile/NavBar.js
+++ src/components/MyProfile/NavBar.js
@@ -2,35 +2,7 @@ import React from "react";
 import "./NavBar.css";
 
 const NavBar = () => {
-  return (
-    <div className="header">
-      <ul className="menu">
-        <li>
-          <label className="opener" htmlFor="drop1">
-            Parent item 1<input type="radio" name="menuopt" id="drop1" />
-          </label>
-          <ul>
-            <li>Menu item 1</li>
-            <li>Menu item 2</li>
-            <li>Menu item 3</li>
-          </ul>
-        </li>
-        <li>
-          <label className="opener" htmlFor="drop2">
-            Parent item 2<input type="radio" name="menuopt" id="drop2" />
-          </label>
-          <ul>
-            <li>Menu item 1</li>
-            <li>Menu item 2</li>
-            <li>Menu item 3</li>
-            <li>Menu item 4</li>
-            <li>Menu item 5</li>
-          </ul>
-          <input type="radio" name="menuopt" id="dropclose" />
-        </li>
-      </ul>
-    </div>
-  );
+  return <div className="header">hi</div>;
 };
 
 export default NavBar;
