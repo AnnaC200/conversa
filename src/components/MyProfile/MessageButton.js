3efe0d931ba256b318352fa55c3ea3f2b04d7b16
Ash Askin <ashaskincodes@gmail.com>
2022-08-24 19:58:32 +0100

    styling finished

--- src/components/MyProfile/MessageButton.js
+++ src/components/MyProfile/MessageButton.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./MessageButton.css";
 
 const MessageButton = () => {
   return (
     <div>
-      <Link to="/Chat">Message</Link>
+      <Link className="messagebutton" to="/Chat">Message</Link>
     </div>
   );
 };
